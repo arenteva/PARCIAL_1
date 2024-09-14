440bf10a7b3c75010f2253b59b17940ceaeeb949
arenteva <retehalo2@hotmail.com>
2024-09-13 21:23:35 -0500

    calcular comision de las ventas  del 10%

--- Vendedor/Vendedor/Vendedor/Program.cs
+++ Vendedor/Vendedor/Vendedor/Program.cs
@@ -6,6 +6,18 @@
         Console.Write("Ingrese el sueldo base del vendedor:  ");
         double sueldoBase = Convert.ToDouble(Console.ReadLine());
 
+        // Solicitar el valor de la ventas
+        Console.Write("Ingrese el valor de la venta 1:  ");
+        double venta1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Ingrese el valor de la venta 2:  ");
+        double venta2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Ingrese el valor de la venta 3:  ");
+        double venta3 = Convert.ToDouble(Console.ReadLine());
+
+
+
 
     }
 }

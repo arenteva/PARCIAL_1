5fe7386a95d52a487f4e20faba1cfe3d44f4abf7
arenteva <retehalo2@hotmail.com>
2024-09-13 21:59:43 -0500

    Verificar las posibles soluciones

--- Coeficiente/Coeficiente/Coeficiente/Program.cs
+++ Coeficiente/Coeficiente/Coeficiente/Program.cs
@@ -2,6 +2,13 @@
 {
     private static void Main(string[] args)
     {
-     // Solicitar los coeficientes a y b de la ecuacion ax + bx = 0
+        // Solicitar los coeficientes a y b de la ecuacion ax + bx = 0
+        Console.Write("Ingrese el coeficiente a: ");
+        double a = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Ingrese el coeficiente b: ");
+        double b = Convert.ToDouble(Console.ReadLine());
+
+        // Verificar las posibles soluciones
     }
 }
